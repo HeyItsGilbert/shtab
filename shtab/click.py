bed533c9b39a4ba965f2ff3320ef0acc0c2807bc
Casper da Costa-Luis <casper.dcl@physics.org>
2026-08-22 01:56:12 +0100

    `shtab.click` first working draft

--- shtab/click.py
+++ shtab/click.py
@@ -1,80 +1,97 @@
-'''
-@main.command(hidden=True)
-@click.pass_context
-def completion(ctx):
-    """Generate bash completion script"""
+"""
+Add "completion" command to `main` click group:
 
-    root = ctx.find_root()
-    rootcmd = root.command
-
-    main_parser = add_to_parser(rootcmd, argparse.ArgumentParser(prog=rootcmd.name))
-
-    print(shtab.complete(main_parser))
-'''
+>>> import shtab.click
+>>> main.command()(shtab.click.completion)
+"""
 import argparse
+import logging
 
 import click
 
-import shtab
+from . import DIRECTORY, FILE, SUPPORTED_SHELLS
+from . import complete as _complete
+
+log = logging.getLogger(__name__)
 
 
-def add_to_parser(command, parser):
+@click.argument('shell', default="bash", type=click.Choice(SUPPORTED_SHELLS))
+@click.option('--prefix', default=None, help="Prepended to generated functions to avoid clashes.")
+@click.option('--preamble', default="", help="Prepended to generated script.")
+@click.pass_context
+def completion(ctx, shell, prefix, preamble):
+    """Print shell completion script."""
+    root = ctx.find_root()
+    print(complete(root.command, shell=shell, root_prefix=prefix, preamble=preamble))
+
+
+def complete(command: click.Command, **kwargs):
+    """
+    Generate a completion script for the given click command.
+
+    kwargs:
+      passed to `shtab.complete()`.
+    """
+    parser = click2argparse(command)
+    return _complete(parser, **kwargs)
+
+
+def _complete_paths(arg, param):
+    if isinstance(param.type, click.File):
+        # for safety, don't complete files in write-only (creation) mode
+        arg.complete = FILE if set("ra+") & set(param.type.mode.lower()) else DIRECTORY
+    elif isinstance(param.type, click.Path):
+        if param.type.file_okay:
+            arg.complete = FILE
+        elif param.type.dir_okay:
+            arg.complete = DIRECTORY
+
+
+def click2argparse(command: click.Command, parser=None):
+    if parser is None:
+        log.debug("parser:%s", command.name)
+        parser = argparse.ArgumentParser(prog=command.name, description=command.help)
     for param in command.params:
-        if param.param_type_name == "option":
-            spec = {}
+        if getattr(param, 'deprecated', False): # click>=8.2
+            continue
+        if getattr(param, 'hidden', False):     # pallets/click#3788
+            continue
 
+        spec = {'help': getattr(param, 'help', None)} # click>=8.5
+        if param.param_type_name == 'option':
+            log.debug("optional:%s", param.opts)
             if param.is_flag:
-                spec["action"] = "store_true"
+                spec['action'] = 'store_true'
             else:
-                spec["nargs"] = param.nargs
-                spec["required"] = param.required
-
+                spec['metavar'] = param.metavar
+                spec['nargs'] = param.nargs
+                spec['required'] = param.required
             if isinstance(param.type, click.Choice):
-                spec["choices"] = param.type.choices
-
+                spec['choices'] = param.type.choices
+            log.debug("spec:%s", spec)
             arg = parser.add_argument(*param.opts, **spec)
-
-            if isinstance(param.type, click.File):
-                arg.complete = shtab.FILE
-            elif isinstance(param.type, click.Path):
-                if param.type.file_okay and not param.type.dir_okay:
-                    arg.complete = shtab.FILE
-                elif not param.type.file_okay and param.type.dir_okay:
-                    arg.complete = shtab.DIRECTORY
-
-        elif param.param_type_name == "argument":
-            spec = {}
-
+            _complete_paths(arg, param)
+        elif param.param_type_name == 'argument':
+            log.debug("positional:%s", param.name)
             if param.nargs == -1:
-                spec["nargs"] = "+" if param.required else "*"
+                spec['nargs'] = '+' if param.required else '*'
             else:
-                spec["nargs"] = param.nargs
-
+                spec['nargs'] = param.nargs
             if isinstance(param.type, click.Choice):
-                spec["choices"] = param.type.choices
-
+                spec['choices'] = param.type.choices
+            log.debug("spec:%s", spec)
             arg = parser.add_argument(param.name, **spec)
+            _complete_paths(arg, param)
 
-            if isinstance(param.type, click.File):
-                arg.complete = shtab.FILE
-            elif isinstance(param.type, click.Path):
-                if param.type.file_okay and not param.type.dir_okay:
-                    arg.complete = shtab.FILE
-                elif not param.type.file_okay and param.type.dir_okay:
-                    arg.complete = shtab.DIRECTORY
-
-    if hasattr(command, "commands") and len(command.commands) > 0:
-        subparsers = parser.add_subparsers()
-        subparsers.required = True
-        subparsers.dest = "subcommand"
-
+    if hasattr(command, 'commands') and len(command.commands) > 0:
+        log.debug("subcommands:%s", command.commands)
+        subparsers = parser.add_subparsers(dest=command.name, help=command.short_help,
+                                           description=command.help, required=True)
         for name, subcmd in command.commands.items():
-            if subcmd.hidden:
+            if subcmd.hidden or getattr(subcmd, 'deprecated', False):       # click>=8.2
                 continue
-
-            # non-empty help necessary or argparse doesn't consider it an action??
-            subparser = subparsers.add_parser(name, help=command.help)
-
-            add_to_parser(subcmd, subparser)
-
+            log.debug("subcommand:%s", name)
+            subparser = subparsers.add_parser(name, help=subcmd.short_help,
+                                              description=subcmd.help)
+            click2argparse(subcmd, subparser)
     return parser
